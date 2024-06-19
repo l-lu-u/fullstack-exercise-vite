d474d6d23e87063e058b340ed14a4b4c66114412
chenl13 <lu.1.chen@aalto.fi>
2024-06-19 15:32:19 +0300

    study part 1a

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log('now, a+b')
-  
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} pluse {b} is {a+b}
-      </p>
+      <p>Hello {props.name}, you have been to the earth for {props.age} years.</p>
     </div>
+  ) 
+}
+
+const Footer =() => {
+  return (
+    <div>
+      Greetin app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [
+    {name: 'Utu', age: 4},
+    {name: 'Ene', age: 10},
+  ]
+
+  console.log(friends)
+  
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name={friends[0].name} age={friends[0].age}/>
+      <Hello name={friends[1].name} age={friends[1].age} />
+      <Footer />
+    </>
   )
 }
 
